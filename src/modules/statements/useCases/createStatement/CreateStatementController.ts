cc018663ef93448453183ade30fba29652843d0b
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2021-12-11 19:19:10 -0300

    feat(Modules/Statements/useCases/createStatement): CreateStatementController.ts. Insert option TRANSFER.

--- src/modules/statements/useCases/createStatement/CreateStatementController.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.ts
@@ -6,6 +6,7 @@ import { CreateStatementUseCase } from './CreateStatementUseCase';
 enum OperationType {
   DEPOSIT = 'deposit',
   WITHDRAW = 'withdraw',
+  TRANSFER = 'transfer'
 }
 
 export class CreateStatementController {
